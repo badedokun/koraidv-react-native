e2ba09fca6ab24174dd808a7bd909593a79bfbce
badedokun <developer@korastratum.com>
2026-07-03 12:52:32 -0400

    v1.10.4: version bump for unified SDK release (native theming/UI; RN delegates to native)

--- build/plugin/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigPlugin } from '@expo/config-plugins';
-interface KoraIDVPluginProps {
-    /** Custom camera permission message for iOS Info.plist */
-    cameraPermissionMessage?: string;
-}
-declare const withKoraIDV: ConfigPlugin<KoraIDVPluginProps | void>;
-export default withKoraIDV;
-//# sourceMappingURL=index.d.ts.map
